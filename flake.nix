d7b3dc7bd1096dbd1a9ef5f5e5b7bcec0b63dfb2
Antidote1911 <antidote1911@gmail.com>
2023-08-16 19:49:31 +0200

    workflow

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    let
+      inherit (builtins) fromTOML readFile;
+      dexiosCargoToml = fromTOML (readFile ./argon2cli/Cargo.toml);
+
+      mkArgon2cli = { lib, rustPlatform, ... }: rustPlatform.buildRustPackage {
+        inherit (argon2cliCargoToml.package) name version;
+
+        src = lib.cleanSource ./.;
+
+        doCheck = true;
+
+        cargoLock.lockFile = ./Cargo.lock;
+      };
+    in
+    {
+      overlays = rec {
+        dexios = final: prev: {
+          dexios = prev.callPackage mkArgon2cli { };
+        };
+        default = argon2cli;
+      };
+    }
+    //
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        argon2cli = pkgs.callPackage mkArgon2cli { };
+      in
+      {
+        # Executes by `nix build .#<name>`
+        packages = {
+          inherit argon2cli;
+          default = argon2cli;
+        };
+        # the same but deprecated in Nix 2.7
+        defaultPackage = self.packages.${system}.default;
+
+        # Executes by `nix run .#<name> -- <args?>` 
+        apps = {
+          argon2cli = {
+            type = "app";
+            program = "${argon2cli}/bin/argon2cli";
+          };
+          default = self.apps.${system}.argon2cli;
+        };
+        # Executes by `nix run . -- <args?>`
+        # the same but deprecated in Nix 2.7
+        defaultApp = self.apps.${system}.default;
+
+        # Used by `nix develop`
+        devShell = with pkgs; mkShell {
+          packages = [ cargo rustc rustfmt clippy rust-analyzer ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+      });
+}
